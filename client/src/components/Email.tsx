8a67f8b61935c8682523a5ee3eee2660b321e65e
Kunal Singh <kunalsin9h@gmail.com>
2023-03-31 22:14:47 +0530

    + emails

--- client/src/components/Email.tsx
+++ client/src/components/Email.tsx
@@ -1,11 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import * as EmailValidator from "email-validator";
+import { client } from "../App";
 
 export function Email() {
+  const [subject, setSubject] = useState("");
+  const [text, setText] = useState("");
+  const [html, setHtml] = useState("");
+  const [emails, setEmails] = useState([""]);
+
+  const [totalEmails, setTotalEmails] = useState(0);
+  const [validEmails, setValidEmails] = useState(0);
+
   return (
-    <div className="text-white text-xl h-full flex justify-center items-center">
-      <div className="rounded p-4 drop-shadow-2xl bg-[#FEC868] text-black">
-        Email (Coming Soon)
+    <div className="">
+      <div className="flex justify-center h-[80%]">
+        <div className="p-4 m-8 grow rounded border-solid border-2 flex flex-col ">
+          <label className="text-white font-bold p-2 m-2">
+            Subject:
+            <input
+              placeholder="Email subject"
+              className="rounded p-2 m-2 w-[60%] ml-4 text-black"
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
+            />
+          </label>
+          <label className="text-white font-bold p-2 m-2">
+            Text Body:
+            <input
+              placeholder="Text email body"
+              className="rounded p-2 m-2 w-[60%] ml-4 text-black"
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+          </label>
+          <label className="text-white font-bold p-2 m-2">
+            HTML Body:
+            <br />
+            <textarea
+              placeholder="HTML email body"
+              rows={15}
+              cols={60}
+              className="rounded p-2 m-2 text-black"
+              onChange={(e) => {
+                setHtml(e.target.value);
+              }}
+            ></textarea>
+          </label>
+        </div>
+        <div className="p-4 m-8 grow rounded border-solid border-2">
+          <span className="text-white font-bold p-2 m-2">
+            Emails: (one per line)
+          </span>
+          <br />
+          <textarea
+            className="rounded p-2 m-2"
+            rows={25}
+            cols={50}
+            onChange={(e) => {
+              const emails = e.target.value.split("\n");
+              setTotalEmails(emails.length);
+
+              const validEmails: string[] = [];
+
+              for (const email of emails) {
+                if (EmailValidator.validate(email)) {
+                  validEmails.push(email);
+                }
+              }
+              setValidEmails(validEmails.length);
+              setEmails(validEmails);
+            }}
+          ></textarea>
+          <br />
+          <span className="text-white p-2 m-2">
+            Total Emails (all entries):{" "}
+            <span className="font-bold rounded bg-white text-black p-1">
+              {totalEmails}
+            </span>
+          </span>
+          <br />
+          <span className="text-white p-2 m-2">
+            Valid Emails:{" "}
+            <span className="font-bold rounded bg-white text-black p-1">
+              {validEmails}
+            </span>
+          </span>
+        </div>
       </div>
+      <button
+        className="w-24 font-bold text-black px-8 py-2 m-8 bg-[#F4C868] hover:bg-[#eda812] rounded hover:scale-110 duration-300 active:bg-black active:text-white "
+        onClick={async (e) => {
+          e.preventDefault;
+
+          console.log(html);
+
+          await client.sendMail.mutate({
+            emails,
+            subject,
+            text,
+            html,
+          });
+        }}
+      >
+        Send
+      </button>
     </div>
   );
 }
