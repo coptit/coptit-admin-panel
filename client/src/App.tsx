e29d6a25aa29ef1266e6a6146fd94953e8d77962
Kunal Singh <kunalsin9h@gmail.com>
2023-03-31 10:28:07 +0530

    bind to 0.0.0.0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ import React from "react";
 export const client = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "https://coptit-back.hop.sh",
+      url: "http://0.0.0.0:4001",
     }),
   ],
 });
@@ -25,7 +25,7 @@ export function App() {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "https://coptit-back.hop.sh",
+          url: "http://0.0.0.0:4001",
         }),
       ],
     })
