57c56ed12cee03fa3f8199173f53f185e4c90431
Kunal Singh <kunalsin9h@gmail.com>
2023-04-01 20:55:34 +0530

    add textarea insted of input for TEXT Body email option

--- client/src/components/Email.tsx
+++ client/src/components/Email.tsx
@@ -14,7 +14,7 @@ export function Email() {
   return (
     <div className="">
       <div className="flex justify-center h-[80%]">
-        <div className="p-4 m-8 grow rounded border-solid border-2 flex flex-col ">
+        <div className="p-4 m-8 grow rounded-xl border-solid border-2 flex flex-col bg-[#1e1915] ">
           <label className="text-white font-bold p-2 m-2">
             Subject:
             <input
@@ -27,20 +27,23 @@ export function Email() {
           </label>
           <label className="text-white font-bold p-2 m-2">
             Text Body:
-            <input
+            <br />
+            <textarea
               placeholder="Text email body"
               className="rounded p-2 m-2 w-[60%] ml-4 text-black"
+              rows={8}
+              cols={60}
               onChange={(e) => {
                 setText(e.target.value);
               }}
-            />
+            ></textarea>
           </label>
           <label className="text-white font-bold p-2 m-2">
             HTML Body:
             <br />
             <textarea
               placeholder="HTML email body"
-              rows={15}
+              rows={10}
               cols={60}
               className="rounded p-2 m-2 text-black"
               onChange={(e) => {
@@ -49,7 +52,7 @@ export function Email() {
             ></textarea>
           </label>
         </div>
-        <div className="p-4 m-8 grow rounded border-solid border-2">
+        <div className="p-4 m-8 grow rounded border-solid border-2 bg-[#1e1915] rounded-xl">
           <span className="text-white font-bold p-2 m-2">
             Emails: (one per line)
           </span>
@@ -90,7 +93,7 @@ export function Email() {
         </div>
       </div>
       <button
-        className="w-24 font-bold text-black px-8 py-2 m-8 bg-[#F4C868] hover:bg-[#eda812] rounded hover:scale-110 duration-300 active:bg-black active:text-white "
+        className="w-26 font-bold text-black px-8 py-2 m-8 bg-[#F4C868] hover:bg-[#eda812] rounded hover:scale-110 duration-300 active:bg-black active:text-white "
         onClick={async (e) => {
           e.preventDefault;
 
